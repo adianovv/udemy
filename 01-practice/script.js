d13bee086572b969d10a2c820ef02aac3dad614f
Vladyslav <vladik.adianov@gmail.com>
2023-10-18 21:42:39 +0300

    add first task

---
+++ 01-practice/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильомов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильомов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
